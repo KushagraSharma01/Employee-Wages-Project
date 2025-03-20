bd946a67f5271260102c958730189974676c03b5
KushagraSharma01 <kushagrasharma582003@gmail.com>
2025-03-20 15:13:01 +0530

    made a correction in absent case

--- CompleteProjectCode.js
+++ CompleteProjectCode.js
@@ -5,6 +5,7 @@ let isPresent = Math.floor(Math.random()*10)%2;
 
 if(isPresent == present){
     console.log("Employee is Present");
+    return;
 }
 else{
     console.log("Employee is not Present");
